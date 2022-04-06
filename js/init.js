870a39ecfe9437aec88eeae2bbf49de5c56abdb7
Dzmitry Tsimashenka <Dzmitry.Tsimashenka@preservica.com>
2022-04-06 19:10:09 +0300

    Update HTML markup.

--- js/init.js
+++ js/init.js
@@ -3,5 +3,10 @@
 
     $('.sidenav').sidenav();
 
+    lightGallery(document.getElementById('light-gallery'), {
+      thumbnail: true,
+      animateThumb: true,
+      showThumbByDefault: false
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
