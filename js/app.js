7b477a0928fa1a538e408e0d617c55c0d3bcb429
Dzmitry Tsimashenka <Dzmitry.Tsimashenka@preservica.com>
2022-10-03 17:42:57 +0300

    - Add menu.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
 (() => {
   // resources/js/app.js
-  document.addEventListener("DOMContentLoaded", function() {
-    var splide = new Splide(".splide", {});
-    splide.mount();
-  });
+  (() => {
+    document.addEventListener("DOMContentLoaded", function() {
+      var splide = new Splide(".splide", {});
+      splide.mount();
+      splide.on("move", (newIndex) => {
+        const nextSlideTitle = `slide-title-${newIndex}`;
+        const nextSlideDescription = `slide-description-${newIndex}`;
+        const slideTitle = document.getElementById(nextSlideTitle);
+        const slideDescription = document.getElementById(nextSlideDescription);
+        slideTitle.classList.add("hidden");
+        slideDescription.classList.add("hidden");
+      });
+      splide.on("moved", (newIndex) => {
+        const nextSlideTitle = `slide-title-${newIndex}`;
+        const nextSlideDescription = `slide-description-${newIndex}`;
+        const slideTitle = document.getElementById(nextSlideTitle);
+        const slideDescription = document.getElementById(nextSlideDescription);
+        slideTitle.classList.remove("hidden");
+        slideTitle.classList.add("animate__bounceInLeft");
+        slideTitle.addEventListener("animationend", () => {
+          slideTitle.classList.remove("animate__bounceInLeft");
+        });
+        slideDescription.classList.remove("hidden");
+        slideDescription.classList.add("animate__bounceInRight");
+        slideDescription.addEventListener("animationend", () => {
+          slideDescription.classList.remove("animate__bounceInRight");
+        });
+      });
+    });
+  })();
 })();
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,37 @@
-document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.splide', {
-        // autoHeight: true,
-    } );
-    splide.mount();
-} );
+(() => {
+    document.addEventListener("DOMContentLoaded", function() {
+        var splide = new Splide(".splide", {});
+        splide.mount();
+
+        splide.on("move", (newIndex) => {
+            const nextSlideTitle = `slide-title-${newIndex}`;
+            const nextSlideDescription = `slide-description-${newIndex}`;
+
+            const slideTitle = document.getElementById(nextSlideTitle);
+            const slideDescription = document.getElementById(nextSlideDescription);
+
+            slideTitle.classList.add("hidden");
+            slideDescription.classList.add("hidden");
+        });
+
+        splide.on("moved", (newIndex) => {
+            const nextSlideTitle = `slide-title-${newIndex}`;
+            const nextSlideDescription = `slide-description-${newIndex}`;
+
+            const slideTitle = document.getElementById(nextSlideTitle);
+            const slideDescription = document.getElementById(nextSlideDescription);
+
+            slideTitle.classList.remove("hidden");
+            slideTitle.classList.add("animate__bounceInLeft");
+            slideTitle.addEventListener("animationend", () => {
+                slideTitle.classList.remove("animate__bounceInLeft");
+            });
+
+            slideDescription.classList.remove("hidden");
+            slideDescription.classList.add("animate__bounceInRight");
+            slideDescription.addEventListener("animationend", () => {
+                slideDescription.classList.remove("animate__bounceInRight");
+            });
+        });
+    });
+})();
