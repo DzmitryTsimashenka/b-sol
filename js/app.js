4d47f588d50a8e2091e87432326a19b9a9282a54
Dzmitry Tsimashenka <Dzmitry.Tsimashenka@preservica.com>
2022-10-04 13:10:11 +0300

    - Add menu.

--- js/app.js
+++ js/app.js
@@ -9,24 +9,32 @@
           return;
         const slideTitle = `slide-title-${newIndex}`;
         const slideDescription = `slide-description-${newIndex}`;
+        const slideButtons = `slide-buttons-${newIndex}`;
         const titleEl = document.getElementById(slideTitle);
         const descriptionEl = document.getElementById(slideDescription);
+        const buttonsEl = document.getElementById(slideButtons);
         titleEl.classList.add("hidden");
         descriptionEl.classList.add("hidden");
+        buttonsEl.classList.add("hidden");
       });
       splide.on("moved", (newIndex, prevIndex) => {
         if (newIndex === prevIndex)
           return;
         const slideTitle = `slide-title-${newIndex}`;
         const slideDescription = `slide-description-${newIndex}`;
+        const slideButtons = `slide-buttons-${newIndex}`;
         const titleEl = document.getElementById(slideTitle);
         const descriptionEl = document.getElementById(slideDescription);
+        const buttonsEl = document.getElementById(slideButtons);
         titleEl.classList.remove("hidden");
         descriptionEl.classList.remove("hidden");
+        buttonsEl.classList.remove("hidden");
         titleEl.classList.add("animate__bounceInLeft");
         descriptionEl.classList.add("animate__bounceInRight");
+        buttonsEl.classList.add("animate__fadeInUpBig");
         titleEl.addEventListener("animationend", () => titleEl.classList.remove("animate__bounceInLeft"), { once: true });
         descriptionEl.addEventListener("animationend", () => descriptionEl.classList.remove("animate__bounceInRight"), { once: true });
+        buttonsEl.addEventListener("animationend", () => buttonsEl.classList.remove("animate__fadeInUpBig"), { once: true });
       });
     });
   })();
