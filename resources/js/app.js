3eccb219848e226a97e702d04ec329cac09962e0
Dzmitry Tsimashenka <Dzmitry.Tsimashenka@preservica.com>
2022-10-05 16:19:47 +0300

    - Add menu.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,110 @@
 (() => {
     document.addEventListener("DOMContentLoaded", function() {
+        // Back to top button.
+        const myBackToTop = function () {
+            // browser window scroll
+            var offset = 300,
+                offset_opacity = 1200,
+                back_to_top = document.querySelector(".back-top"),
+                scrollpos = window.scrollY;
+
+            var add_class_back_scroll = function add_class_back_scroll() {
+                back_to_top.classList.add("block");
+                back_to_top.classList.remove("hidden");
+            };
+
+            var add_class_offset_scroll = function add_class_offset_scroll() {
+                back_to_top.classList.add("opacity-90");
+            };
+
+            var remove_class_back_scroll = function remove_class_back_scroll() {
+                back_to_top.classList.remove("block","opacity-90");
+                back_to_top.classList.add("hidden");
+            };
+
+            // back to top by es6-scroll-to
+            var defaults = {
+                duration: 400,
+                easing: function easing(t, b, c, d) {
+                    return -c * (t /= d) * (t - 2) + b;
+                },
+                to: 0
+            };
+            var animatedScrollTo = function animatedScrollTo(args) {
+                if (isInteger(args)) {
+                    args = {
+                        to: args
+                    };
+                }
+                var options = extend(defaults, args);
+                options.startingYOffset = window.pageYOffset;
+                options.distanceYOffset = parseInt(options.to, 10) - options.startingYOffset;
+                window.requestAnimationFrame(function (timestamp) {
+                    return animateScroll(options, timestamp);
+                });
+            };
+            var animateScroll = function animateScroll(options, now) {
+                if (!options.startTime) {
+                    options.startTime = now;
+                }
+                var currentTime = now - options.startTime;
+                var newYOffset = Math.round(options.easing(currentTime, options.startingYOffset, options.distanceYOffset, options.duration));
+                if (currentTime < options.duration) {
+                    window.requestAnimationFrame(function (timestamp) {
+                        return animateScroll(options, timestamp);
+                    });
+                } else {
+                    newYOffset = options.to;
+                }
+                setScrollTopPosition(newYOffset);
+            };
+            var setScrollTopPosition = function setScrollTopPosition(newYOffset) {
+                document.documentElement.scrollTop = newYOffset;
+                document.body.scrollTop = newYOffset;
+            };
+            var isInteger = function isInteger(value) {
+                if (Number.isInteger) {
+                    return Number.isInteger(value);
+                } else {
+                    return typeof value === 'number' && isFinite(value) && Math.floor(value) === value;
+                }
+            };
+            var extend = function extend(defaults, options) {
+                var extendedOptions = {};
+                for (var key in defaults) {
+                    extendedOptions[key] = options[key] || defaults[key];
+                }
+                return extendedOptions;
+            };
+            var easeInQuint = function easeInQuint(t, b, c, d) {
+                return c * (t /= d) * t * t * t * t + b;
+            };
+
+            const scroll_a = document.querySelectorAll('.back-top');
+            if ( scroll_a !=null) {
+                for (var i = 0; i < scroll_a.length; i++) {
+                    scroll_a[i].addEventListener("click", function(){
+                        animatedScrollTo({
+                            easing: easeInQuint,
+                            duration: 800
+                        });
+                    });
+                }
+            }
+
+            window.addEventListener('scroll', function () {
+                scrollpos = window.scrollY;
+                if (scrollpos > offset) {
+                    add_class_back_scroll();
+                } else {
+                    remove_class_back_scroll();
+                }
+                if (scrollpos > offset_opacity) {
+                    add_class_offset_scroll();
+                }
+            });
+        }
+
         // Lightbox.
         const myLightbox = () => {
             const lightbox_class = document.querySelector(".glightbox3");
@@ -61,6 +166,7 @@
             });
         }
 
+        myBackToTop();
         mySplide();
         myLightbox();
     });
